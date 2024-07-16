54e78d1c9a252c5e302a5f6d74684743b9fc1e39
somaia96 <somaia96.sh@gmail.com>
2024-07-17 00:37:13 +0300

    Ads

--- src/components/Home/Best.tsx
+++ src/components/Home/Best.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Best = () => {
   return (
     <div className='flex gap-8 flex-col md:flex-row'>
-   
-   <div className='flex-1 text-right flex justify-start items-center text-xl bg-[url("../../../src/assets/images/Home/32.png")]'>
+
+      <div className='flex-1 text-right flex justify-start items-center text-xl bg-[url("../../../src/assets/images/Home/32.png")]'>
         <div className='flex-1'>
           <span className='text-gray-500'>سماعات ديجيتال</span>
-        <h2 className='text-2xl font-bold'>أفضل صوت نقي</h2>
-        <span>تسوق الان</span>
+          <h2 className='text-2xl font-bold'>أفضل صوت نقي</h2>
+          <span>تسوق الان</span>
         </div>
         <div className='hidden lg:block lg:flex-1'></div>
         <div className='flex-1 h-[240px]'></div>
@@ -18,8 +18,8 @@ const Best = () => {
       <div className='flex-1 text-right flex justify-start items-center text-xl bg-[url("../../../src/assets/images/Home/16.png")]'>
         <div className='flex-1'>
           <span className='text-gray-500'>سماعات ديجيتال</span>
-        <h2 className='text-2xl font-bold'>أفضل صوت نقي</h2>
-        <span>تسوق الان</span>
+          <h2 className='text-2xl font-bold'>أفضل صوت نقي</h2>
+          <span>تسوق الان</span>
         </div>
         <div className='hidden lg:block lg:flex-1'></div>
         <div className='flex-1 h-[240px]'></div>
