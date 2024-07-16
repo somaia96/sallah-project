b762077886060086ddcdf37fa58396b7e72b6816
somaia96 <somaia96.sh@gmail.com>
2024-07-17 00:12:53 +0300

    Featured

--- src/components/Home/Featured.tsx
+++ src/components/Home/Featured.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Head from './Head'
+import Products from '../products/Products'
 
 const Featured = () => {
   return (
-    <div>
+    <div className='py-5'>
             <Head title="منتجات خاصة" description='تسوق احدث المنتجات المميزة المضافة جديد'/>
+            <Products endIndex={12} grid={1}/>
 
     </div>
   )
