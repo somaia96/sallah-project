d05f5e3dd767da5015c35d32f51c9beccb864a8b
somaia96 <somaia96.sh@gmail.com>
2024-07-17 03:31:31 +0300

    BestSale

--- src/components/Home/BestSale.tsx
+++ src/components/Home/BestSale.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
+import { Button } from '../ui/button'
 
 const BestSale = () => {
   return (
-    <div className='bg-bottom bg-cover bg-no-repeat flex justify-center items-center bg-[url("../../src/assets/images/Home/22.png")]'>
-       <div className="container flex">
-       <div className='flex-1'>
-        
-       </div>
-       <div className='flex-1'>
-
-       </div>
+    <div className='bg-bottom py-[--spacing-3xl] bg-cover bg-no-repeat flex justify-center items-center bg-[url("../../src/assets/images/Home/22.png")]'>
+      <div className="container gap-5 flex-col md:flex-row flex justify-between items-center">
+        <div className='flex-1 order-1'>
+          <img src="../../src/assets/images/Home/0.png" alt="" />
+        </div>
+        <div className='flex-1 flex md:order-2 items-center md:items-start flex-col gap-5' dir='rtl'>
+          <h1 className='font-bold text-4xl text-white'>افضل التخفيضات 2022</h1>
+          <p className='text-gray-50 text-xl'>متجر سلة يوفر لك كل ماتحتاجه من إلكترونيات أو أثاث منزلي بالإضافة إلى أفضل التحفيضات على المنتجات , تسوق الان واستمتع بكل بالتخفيضات على المنتجات</p>
+          <Button className='w-[fit-content] bg-[--primary] hover:text-[--primary] text-xl'>اكتشف المزيد</Button>
+        </div>
       </div>
-      
+
     </div>
   )
 }
